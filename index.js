5909b85ced48949d21ae730a4656cd947fddddb2
Nicolas Marini <nmarini44@gmail.com>
2019-04-23 01:36:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  const newObj = Object.assign({}, driver, key)
+  const newObj = Object.assign({}, driver)
   delete newObj.key
   return newObj
 }
